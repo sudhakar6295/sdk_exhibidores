4ae987ddc87ed3b797228df946bd8a515875cb40
sudhakar6295 <53999168+sudhakar6295@users.noreply.github.com>
2024-07-28 21:01:13 +0530

    Update models.py

--- scraping/exhibidoreschile_scraping/models.py
+++ scraping/exhibidoreschile_scraping/models.py
@@ -4,7 +4,7 @@ from sqlalchemy.sql import func
 
 Base = declarative_base()
 
-class Product(Base):
+class Products(Base):
     __tablename__ = 'products'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
